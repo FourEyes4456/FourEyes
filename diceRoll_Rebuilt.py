fc9bbe7c22245893c65fbab718dbac14b6a2f427
FourEyes4456 <levikupec283@gmail.com>
2024-09-08 14:06:32 -0400

    removed old comments and extraneous code

--- diceRoll_Rebuilt.py
+++ diceRoll_Rebuilt.py
@@ -1,19 +1,14 @@
 import random
-import keyboard
 
 diceType = 0
 diceAmount = 0
 diceModifier = 0
-# Variables for the dice
 rollCount = 0
 singleRoll = 0
 finalRoll = 0
 averageRoll = 0
-# Variables for the rolling process
 correct = ""
-# Confirmation to roll
 diceCounting = True
-# Enables counting of the individual dice rolls
 
 
 def diceInput():
@@ -22,14 +17,11 @@ def diceInput():
     diceAmount = int(input("Please enter dice amount: "))
     diceModifier = int(input("Please enter any modifier (enter 0 if none): "))
     diceCheck = ("\n(y/n) Is this correct? " + str(diceAmount) + "d" + str(diceType))
-    # Inputs are finished and check text is generated
     while True:
         if diceModifier >> 0:
             correct = input(diceCheck + "+" + str(diceModifier) + "  ")
-            # Line for if a modifier is added
         else:
             correct = input(diceCheck + "  ")
-            # Line for if a modifier is not added
         if correct == "y" or "n":
             print()
             break
@@ -37,10 +29,8 @@ def diceInput():
             print()
     if correct == "y":
         diceRolling()
-        # Roll the dice
     else:
         print()
-        # Wrong dice
 
 
 
@@ -49,27 +39,20 @@ def diceRolling():
     while diceCounting is True:
         if int(rollCount) <= int(diceAmount)-1:
             singleRoll = random.randint(1, int(diceType))
-            # Rolls a single dice
             finalRoll += singleRoll
-            # Adds the last roll to the total roll
             print(str(rollCount+1) + ": " + str(singleRoll) + " -> " + str(finalRoll))
-            # Prints: Dice number, Number rolled, Total amount rolled
             rollCount += 1
         else:
             diceCounting = False
-            # Breaks the loop after the proper number of dice is rolled
             finalRoll += diceModifier
-            # Adding the modifier to the final roll
             if diceModifier >> 0:
                 print("Modifier: " + str(diceModifier) + " -> " + str(finalRoll))
-                # Adds another line to the diceCounting if there is a modifier
     diceAddup()
 
 
 def diceAddup():
     global diceModifier, diceAmount, finalRoll, averageRoll
     averageRoll = (finalRoll - diceModifier) // diceAmount
-    # Calculating the average roll
     print("\nFinal roll: " + str(finalRoll))
     print("Average roll: " + str(averageRoll))
     rollAgain()
