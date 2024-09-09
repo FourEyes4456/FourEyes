19c94d6c8d346370511867fbe295e18189413d48
FourEyes4456 <levikupec283@gmail.com>
2024-09-09 13:31:10 -0400

    test file modified

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("this is a test")
+state = input("Enter your state: ")
+city = input("Enter your city: ")
+streetName = input("Enter your street name: ")
+zipCode = input("Enter your zip code: ")
+number = input("Enter your house number: ")
+
+print("Your mailing address is:\n", number, streetName, "\n", city, ",", state, zipCode)
